99066ceb2acf658ba5f99ad2c84d8187f7092007
DEANDEV\Dean <dean.stevenson.home@hotmail.com>
2018-02-11 18:02:31 -0800

    escape to exit

--- XNARTS/Game1.cs
+++ XNARTS/Game1.cs
@@ -64,9 +64,8 @@ namespace XNARTS
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
+            if ( GamePad.GetState( PlayerIndex.One ).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown( Keys.Escape ) )
+                Exit();
 
             angle += 0.01f;
             view = Matrix.CreateLookAt(
