1cd8c07ff4794fd67ce8e0f620a28ae7e1843127
A\Dean <dean.stevenson.home@hotmail.com>
2018-02-18 22:57:49 -0800

    cleanup

--- XNARTS/Game1.cs
+++ XNARTS/Game1.cs
@@ -21,18 +21,11 @@ namespace XNARTS
     {
         GraphicsDeviceManager mGraphics;
 
-        BasicEffect basicEffect;
-		BasicEffect basicEffect2;
 		BasicEffect mBasicEffect_World;
 		BasicEffect mBasicEffect_Screen;
+
 		SimpleDraw  mSimpleDraw_World;
         SimpleDraw  mSimpleDraw_Screen;
-
-        Matrix world = Matrix.CreateTranslation(0, 0, 0);
-        Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 3), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
-        Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.01f, 100f);
-        double angle = 0;
-		float mCamDrift, mCamStartX, mCamStartY;
 
 		XNARTSMouse mMouse;
 
@@ -40,10 +33,6 @@ namespace XNARTS
         {
             mGraphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-
-			mCamDrift = 0.1f;
-			mCamStartX = 6.0f;
-			mCamStartY = 4.0f;
 		}
 
 		/// <summary>
@@ -79,7 +68,7 @@ namespace XNARTS
 			// world space rendering setup
 			mBasicEffect_World = new BasicEffect( GraphicsDevice );
 			mBasicEffect_World.World = Matrix.Identity;
-			mBasicEffect_World.View = Matrix.CreateLookAt( new Vector3( mCamStartX, mCamStartY, 1f ), new Vector3( mCamStartX, mCamStartY, 0f ), new Vector3( 0f, 1f, 0f ) );
+			mBasicEffect_World.View = Matrix.CreateLookAt( new Vector3( 6, 4, 1f ), new Vector3( 6, 4, 0f ), new Vector3( 0f, 1f, 0f ) );
 			float aspect = (float)(mGraphics.PreferredBackBufferHeight) / mGraphics.PreferredBackBufferWidth;
 			float viewport_scale = 10f;
 			mBasicEffect_World.Projection = Matrix.CreateOrthographicOffCenter( -viewport_scale, viewport_scale, -viewport_scale * aspect, viewport_scale * aspect, 0f, 2f );
@@ -89,9 +78,6 @@ namespace XNARTS
 			mBasicEffect_Screen.World = Matrix.Identity;
 			mBasicEffect_Screen.View = Matrix.CreateLookAt( new Vector3( 0f, 0f, 1f ), new Vector3( 0f, 0f, 0f ), new Vector3( 0f, 1f, 0f ) );
 			mBasicEffect_Screen.Projection = Matrix.CreateOrthographicOffCenter( 0, mGraphics.PreferredBackBufferWidth, mGraphics.PreferredBackBufferHeight, 0, 0f, 2f );
-
-			basicEffect = new BasicEffect(GraphicsDevice);
-			basicEffect2 = new BasicEffect( GraphicsDevice );
 		}
 
 		/// <summary>
@@ -109,9 +95,6 @@ namespace XNARTS
         /// <param name="game_time">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime game_time)
         {
-			float dx = mCamDrift * (game_time.TotalGameTime.Seconds + (game_time.TotalGameTime.Milliseconds) / 1000f);
-			mBasicEffect_World.View = Matrix.CreateLookAt( new Vector3( mCamStartX + dx, mCamStartY, 1f ), new Vector3( mCamStartX + dx, mCamStartY, 0f ), new Vector3( 0f, 1f, 0f ) );
-
 			// user controller app exit
 			if ( GamePad.GetState( PlayerIndex.One ).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown( Keys.Escape ) )
             {
@@ -120,12 +103,6 @@ namespace XNARTS
 
             // TODO: Add your update logic here
             mMouse.Update( game_time );
-
-            angle += 0.01f;
-            view = Matrix.CreateLookAt(
-                new Vector3(5 * (float)Math.Sin(angle), -2, 5 * (float)Math.Cos(angle)),
-                new Vector3(0, 0, 0),
-                Vector3.UnitY);
 
             base.Update(game_time);
         }
@@ -136,82 +113,20 @@ namespace XNARTS
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw( GameTime game_time )
         {
-            // A temporary array, with 12 items in it, because
-            // the icosahedron has 12 distinct vertices
-            VertexPositionColor[] vertices = new VertexPositionColor[2];
-			VertexPositionColor[] vertices2 = new VertexPositionColor[2];
-
-			// vertex position and color information for icosahedron
-			vertices[ 0] = new VertexPositionColor(new Vector3(-0.26286500f, 0.0000000f, 0.42532500f), Color.Red);
-            vertices[1] = new VertexPositionColor(new Vector3(0.26286500f, 0.0000000f, 0.42532500f), Color.Orange);
-
-			vertices2[ 0 ] = new VertexPositionColor( new Vector3( -0.3f, 0.0000000f, 0.4f ), Color.Blue );
-			vertices2[ 1 ] = new VertexPositionColor( new Vector3( 0.23f, 0.0000000f, 0.41f ), Color.Green );
-
-			var vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 2, BufferUsage.WriteOnly);
-            vertexBuffer.SetData<VertexPositionColor>(vertices);
-
-			var vertexBuffer2 = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 2, BufferUsage.WriteOnly);
-			vertexBuffer2.SetData<VertexPositionColor>( vertices2 );
-
-			short[] indices = new short[4];
-            indices[0] = 0;
-            indices[1] = 1;
-            indices[2] = 2;
-            indices[3] = 3;
-
-            var indexBuffer = new IndexBuffer(mGraphics.GraphicsDevice, typeof(short), indices.Length, BufferUsage.WriteOnly);
-            indexBuffer.SetData(indices);
-
-
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            basicEffect.World = world;
-            basicEffect.View = view;
-            basicEffect.Projection = projection;
-            basicEffect.VertexColorEnabled = true;
-
-			basicEffect2.World = world;
-			basicEffect2.View = view;
-			basicEffect2.Projection = projection;
-			basicEffect2.VertexColorEnabled = true;
-
-			GraphicsDevice.SetVertexBuffer(vertexBuffer);
-            GraphicsDevice.Indices = indexBuffer;
 
             RasterizerState rasterizerState = new RasterizerState();
             rasterizerState.CullMode = CullMode.None;
             GraphicsDevice.RasterizerState = rasterizerState;
-
-            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                // GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 20);
-                GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.LineList, 0, 0, 20);
-            }
-
-			GraphicsDevice.SetVertexBuffer( vertexBuffer2 );
-
-			foreach( EffectPass pass in basicEffect2.CurrentTechnique.Passes )
-			{
-				pass.Apply();
-				// GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 20);
-				GraphicsDevice.DrawIndexedPrimitives( PrimitiveType.LineList, 0, 0, 20 );
-			}
 
 			// simple draw only clients
 			mMouse.RenderWorld( game_time );
 			mMouse.RenderScreen( game_time );
 
 			mBasicEffect_World.VertexColorEnabled = true;
-			EffectTechnique effectTechnique = mBasicEffect_World.Techniques[0];
-			EffectPassCollection effectPassCollection = effectTechnique.Passes;
 
-
-			//foreach ( EffectPass pass in effectPassCollection )
 			foreach ( EffectPass pass in mBasicEffect_World.CurrentTechnique.Passes )
 			{
-				// hopefully only one pass
 				pass.Apply();
 
 				// actually render simple draw stuff.  possible layers needed.
@@ -220,16 +135,12 @@ namespace XNARTS
 				// render clients who do their own rendering.  they should probably have pre-renders like simple draw, especially if there is more than one pass.
 			}
 
-
 			// simple draw screen
 			mBasicEffect_Screen.VertexColorEnabled = true;
-			effectTechnique = mBasicEffect_Screen.Techniques[ 0 ];
-			effectPassCollection = effectTechnique.Passes;
 
 			//foreach ( EffectPass pass in effectPassCollection )
 			foreach( EffectPass pass in mBasicEffect_Screen.CurrentTechnique.Passes )
 			{
-				// hopefully only one pass
 				pass.Apply();
 
 				// actually render simple draw stuff.  possible layers needed.
