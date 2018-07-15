0d49062681833d3d86902b5bc693c2b61da1a552
DESKTOP-J2CSTIU\deans <dean.stevenson.home@hotmail.com>
2018-07-14 23:41:40 -0700

    work in aabb to camera.

--- XNARTS/Render/Camera.cs
+++ XNARTS/Render/Camera.cs
@@ -19,71 +19,56 @@ namespace XNARTS
 	public class XWorldCam : XICamera
 	{
 		private float mAspect;
-		private Matrix mView;
-		private Matrix mProjection;
+		private xCoord mWorldSize;
+		private xAABB2 mWorldView; 
+		private Matrix mViewMatrix;
+		private Matrix mProjectionMatrix;
 
 
 		public XWorldCam( xCoord screen_dim )
 		{
+			mAspect = ((float)(screen_dim.y)) / screen_dim.x;
+			mWorldSize = XWorld.Instance().GetMapSize();
+			xAABB2 world_view = new xAABB2( new Vector2( 0, 0 ), new Vector2( mWorldSize.x, mWorldSize.y ) );
+			mWorldView = ClampWorldView( world_view );
 			NewWay( screen_dim );
 		}
 
 
 		private void NewWay( xCoord screen_dim )
 		{
-			mAspect = ((float)(screen_dim.y)) / screen_dim.x;
-			xCoord world_size = XWorld.Instance().GetMapSize();
-
-			// new way should work
-			//Vector3 pos = new Vector3( world_size.x * 0.5f, world_size.y * 0.5f, 1f );
-			//Vector3 target = pos - 2f * Vector3.UnitZ;
-
 			Vector3 pos = new Vector3( 0, 0, 1f );
 			Vector3 target = pos - 2f * Vector3.UnitZ;
 
-			mView = Matrix.CreateLookAt( pos, target, Vector3.UnitY );
+			mViewMatrix = Matrix.CreateLookAt( pos, target, Vector3.UnitY );
 
 			// left, right, bottom, top, near, far
-			float left = 0;
-			float right = world_size.x;
-			float top = 0;
-			float bottom = mAspect * world_size.x;
+			float left = mWorldView.GetMin().X;
+			float right = mWorldView.GetMax().X;
+			float top = mWorldView.GetMin().Y;
+			float bottom = mAspect * (mWorldView.GetMax().X - mWorldView.GetMin().X);
 			float near = 1;
 			float far = -1;
-			float x_offset = 1;
-			float y_offset = -1;
-			mProjection = Matrix.CreateOrthographicOffCenter( left + x_offset, right + x_offset, bottom + y_offset, top + y_offset, near, far );
-		}
 
-
-		private void OldWay( xCoord screen_dim )
-		{
-			mAspect = ((float)(screen_dim.y)) / screen_dim.x;
-			//xCoord world_size = XWorld.Instance().GetMapSize();
-
-			// temporary
-			float viewport_scale = 10f;
-			// old hack
-			mView = Matrix.CreateLookAt( new Vector3( 6, 4, 1f ), new Vector3( 6, 4, 0f ), new Vector3( 0f, 1f, 0f ) );
-
-			// new way should work
-			//Vector3 pos = new Vector3( screen_dim.x * 0.5f, screen_dim.y * 0.5f, 1f );
-			//Vector3 target = pos - 2f * Vector3.UnitZ;
-
-			//mView = Matrix.CreateLookAt( pos, target, Vector3.UnitY );
-			mProjection = Matrix.CreateOrthographicOffCenter( -viewport_scale, viewport_scale, -viewport_scale * mAspect, viewport_scale * mAspect, 0f, 2f );
+			mProjectionMatrix = Matrix.CreateOrthographicOffCenter( left, right, bottom, top, near, far );
 		}
 
 
 		Matrix XICamera.GetViewMatrix()
 		{
-			return mView;
+			return mViewMatrix;
 		}
 
 
 		Matrix XICamera.GetProjectionMatrix()
 		{
-			return mProjection;
+			return mProjectionMatrix;
+		}
+
+
+		private xAABB2 ClampWorldView( xAABB2 world_view )
+		{
+			return world_view;
 		}
 
 
