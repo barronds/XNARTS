3621b093d012b4a9c120c5e89cd8ea23aeae00f5
DESKTOP-J2CSTIU\deans <dean.stevenson.home@hotmail.com>
2019-08-06 18:00:51 +0200

    add file, wip for button2

---
+++ XNARTS/UI/Button2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace XNARTS
+{
+	public partial class XUI
+	{
+		/*
+		public class Button : Panel
+		{
+			private String  mText;
+			private Panel   mPanel; // this panel has a label
+
+			public Button( Widget parent, eStyle style, String text, Vector2 pos )
+				: base( )
+			{
+
+			}
+
+			public Button( Widget parent, eStyle style, String text, ePlacement placement )
+			{
+
+			}
+		}
+		*/
+	}
+}
